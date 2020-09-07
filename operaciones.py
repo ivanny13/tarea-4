aa3cb0f4f2fdc3c3078c6c7a300285ef27c4aaf6
Ivannya Cifuentes <ivannya.cifuentes.r@mail.pucv.cl>
2020-09-06 22:42:29 -0300

    operaciones

--- operaciones.py
+++ operaciones.py
@@ -2,3 +2,9 @@ x=8+5
 print(f'La suma de 8 y 5 es {x}')
 y=11-4
 print(f'La resta entre 11 y 4 es {y}')
+z=9*3
+print(f'La multiplicación entre 9 y 3 es {z}')
+w=9/4
+print(f'La división entre 9 y 4 es {w}')
+c=9%4
+print(f'El módulo de 9%4 es {c}')
