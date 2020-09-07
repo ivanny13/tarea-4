3ba6fedc7076dacae3c03a5db9c5673f2e0114ec
Ivannya Cifuentes <ivannya.cifuentes.r@mail.pucv.cl>
2020-09-06 23:25:52 -0300

    lista

---
+++ basededatos.py
@@ -0,0 +1,5 @@
+alumno1={'nombre': 'Ariana', 'nota programación': 6.3, 'nota física': 5.9}
+alumno2={'nombre': 'Javier', 'nota programación': 4.0, 'nota física': 5.0}
+alumno3={'nombre': 'Chayanne', 'nota programación': 7.0, 'nota física': 7.0}
+alumno4={'nombre': 'Maluma', 'nota programación': 1.3, 'nota física':1.0}
+print(alumno3)
